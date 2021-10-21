753c79291ac1261f7ef9ed8bd4fe3213f2e3b9f2
Atlinx <atlinuxtm@gmail.com>
2021-10-21 01:00:02 -0400

    fix nav bar

--- res/responsivenav.js
+++ res/responsivenav.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const hamburger = document.getElementById("hamburgera");
-const navbar = document.getElementById("navbar");
-
-hamburger.addEventListener("click", () => {
-    navbar.className = navbar.className === "nav" ? "responsive" : "nav";
-});
-
-window.addEventListener("resize", () => {
-    if(window.innerWidth >= 1540) {
-        navbar.className = "nav";
-    }
-=======
 const hamburger = document.getElementById("hamburgera");
 const navbar = document.getElementById("navbar");
 
@@ -22,5 +9,4 @@ window.addEventListener("resize", () => {
     if(screen.orientation === "landscape-primary" || screen.orientation === "landscape-secondary") {
         navbar.className = "nav";
     }
->>>>>>> e57da25684577ec5763d4980ebeeb7421bffc0f4
 });
